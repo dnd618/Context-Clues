f37ce9c65eb05dce06ac2b3e66f2a40c84c5a04b
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-07-05 13:45:52 -0500

    completed project

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ var loc = ['Liv', 'Bellagio', 'South Beach', 'Los Angeles', 'Atlanta', 'Hollywoo
 var weaponry = ['kusanagi', 'stapler', 'bazooka', 'nike', 'machete', 'bebe gun', 'fairy dust', 'scissorhand', 'kryptonite', 'yamaha', 'teleporter','silver stake', 'scimitar', 'katana', 'dart', 'blowtorch', 'stun gun', 'taser', 'excalibur', 'AK47'];
 
 document.addEventListener('DOMContentLoaded', function(){
-for(var i = 1; i < 101; i++){
-    console.log(homie[i]+":");
+    for(var i = 1; i < 101; i++){
+    console.log(i);
     var heading = document.createElement('h3')
-    heading.className = "header";
-    document.body.append(heading)
-    var clues = "Clue" + i
-    heading.append(clues)
+    heading.innerHTML = 'Clue ' + i;
+    //Binding arrays to a set
+    var set = {
+        num: i,
+        homie : homie[Math.floor(Math.random() * homie.length)],
+        loc : loc[Math.floor(Math.random() * loc.length)],
+        weaponry : weaponry[Math.floor(Math.random() * weaponry.length)]
+    
+    };
+    //Binding called content to object
+    heading.addEventListener('click', config.bind(set));
+    document.body.appendChild(heading);
     }
 });
-
+//Allows .this to find keyword
+function config() {
+    alert(this.homie + ' did it in ' + this.loc + ' with a ' + this.weaponry);
+}
