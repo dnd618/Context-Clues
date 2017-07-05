5643dd9a1786d62acfa26672fd4ad8ef70bba9bd
dnd628 <IamBham2@InnovateBhams-MacBook-Pro.local>
2017-07-04 23:48:24 -0500

    added a few started adding arrays

---
+++ Desktop/labs/Context Clues/script.js
@@ -0,0 +1,6 @@
+var homie = ['BriBri', 'Playa', 'Vito', 'Kim K', 'Cocheta'];
+
+var loc = ['Liv', 'Bellagio', 'South Beach', 'Los Angeles', 'Atlanta', 'Hollywood', 'New Orleans', 'Denver', 'Nashville', 'Dubai'];
+
+var thangz = ['Dirty Dee', 'Purple Prince', 'Sue Johanson', 'Bruno Mars', '', 'Sang Purple Rain', 'Bought a unicorn with 5000 pennies', 'Dressed up like Edwards Scissorhands', 'Inhaled helium', 'Hid a body', 'Teleported to the 80s',];
+//Add 9 more things..
